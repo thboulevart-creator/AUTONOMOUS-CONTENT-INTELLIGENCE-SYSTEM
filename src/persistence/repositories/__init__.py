dd934ce3d4bb27dcafa59e80c2c977f61b758873
thboulevart-creator <th.boulevart@gmail.com>
2026-08-31 22:04:03 +0200

    chore: expose provenance persistence adapter

--- src/persistence/repositories/__init__.py
+++ src/persistence/repositories/__init__.py
@@ -3,6 +3,7 @@ from .learning import LearningRepository
 from .performance import PerformanceRepository
 from .experiment import ExperimentRepository
 from .decision import DecisionRepository
+from .provenance import ProvenanceRepository
 
 __all__ = [
     "EvidenceRepository",
@@ -10,4 +11,5 @@ __all__ = [
     "PerformanceRepository",
     "ExperimentRepository",
     "DecisionRepository",
+    "ProvenanceRepository",
 ]
